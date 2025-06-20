dce986c6904db8232f7f65bdff8c1c15cd71a5a0
Sebastián Mariano Lopez Cañete <152093811+Barna2811@users.noreply.github.com>
2025-06-19 20:47:27 -0500

    Delete web-apoyo-psicologico directory

--- web-apoyo-psicologico/apoyo_psicologico/quienes_somos.php
+++
@@ -1,9 +0,0 @@
-<?php include 'includes/header.php'; ?>
-
-<section class="content-section">
-    <h2>Quiénes Somos</h2>
-    <p>Somos un equipo dedicado a promover el bienestar mental y a proporcionar un espacio seguro para quienes buscan apoyo. Creemos que la salud mental es tan importante como la salud física y que todos merecen acceso a recursos y orientación.</p>
-    <p>Nuestra misión es desestigmatizar los problemas de salud mental y empoderar a las personas para que busquen la ayuda que necesitan.</p>
-    </section>
-
-<?php include 'includes/footer.php'; ?>
